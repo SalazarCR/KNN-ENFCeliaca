0d1cc147212ec626ed5461ce195e10d130eab955
SalazarCR <2213110208@untels.edu.pe>
2025-05-11 23:02:56 -0500

    modificando

--- Django/ENFCeliaca/primeraWebApp/urls.py
+++ Django/ENFCeliaca/primeraWebApp/urls.py
@@ -2,7 +2,7 @@
 URL configuration for primeraWebApp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from WebApp.views import portada_view, diagnostico_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', portada_view, name='portada'),
+    path('diagnostico/', diagnostico_view, name='diagnostico'),
 ]
+
+
+
